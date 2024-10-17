d5ffce071d5d537ab2b1e568b1d5229f74209d35
Md Ayaz <ayazkh1226@gmail.com>
2024-10-17 11:18:08 -0700

    tmdb data is fetched and main container is done

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { API_OPTIONS } from '../utils/constants';
 
-const VideoBackground = ({movidId}) => {
+import { useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
 
-    const [trailerId, setTrailerId] = useState(null);
 
-    const getMovieVideo = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/933260/videos?language=en-US', API_OPTIONS);
-        const json = await data.json();
-        console.log(json);
-        
-        const filterData = json.results.filter((video) => video.type === "Trailer")
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        console.log(trailer);
-        setTrailerId(trailer.key);
+const VideoBackground = ({movieId}) => {
 
-    }
-
-    useEffect(() => {
-        getMovieVideo();
-    }, [])
-
+    const trailerVideo = useSelector(store => store.movies?.trailerVideo); 
+    useMovieTrailer(movieId);
+    
   return (
-    <div><iframe width="560" height="315" src={"https://www.youtube.com/embed/"+ trailerId} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+    <div className='w-screen'
+    >
+      <iframe className='w-screen aspect-video' 
+      src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?&autoplay=1&mute=1"} title="YouTube video player"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
   )
 }
 
