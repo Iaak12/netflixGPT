a822f38dd7d335728f4f53450151118d2436b4e0
Md Ayaz <ayazkh1226@gmail.com>
2024-10-22 23:45:41 -0700

    multi language feature is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,8 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const user = useSelector(store => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch)
+
   const handleSignOut = () => {
     signOut(auth).then(() => {}).catch((error) => {
        navigate("/error");
@@ -50,11 +52,13 @@ const handleLanguageChange = (e) => {
     <div className='absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10'>
         <img className='w-44' alt='logo' src={LOGO} />
       { user && (  <div className='flex p-3'>
+        {showGptSearch && (
            <select className='p-2 m-2 bg-gray-900 text-white' onChange={handleLanguageChange}>
             {SUPPORTED_LANGUAGES.map(lang =>  <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
            
            </select>
-          <button onClick={handleGptSearchClick} className='py-2 px-4 mx-4 my-2 rounded-lg bg-purple-800 text-white'>GPT Search</button>
+           )}
+          <button onClick={handleGptSearchClick} className='py-2 px-4 mx-4 my-2 rounded-lg bg-purple-800 text-white'>{showGptSearch ? "Homepage" : "GPT Search"}</button>
           <img className='w-12 h-12' alt='usericon' src={user?.photoURL}/>
           <button onClick={handleSignOut} className='font-bold text-white'>(Sign Out)</button>
         </div>
